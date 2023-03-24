a7b7cd7b7bfb144b37b54ea82c7725d8a8b0999d
Oleksandr Levchuk <73852716+alxlvgit@users.noreply.github.com>
2023-03-23 22:52:41 -0700

    refactor passport configuration

--- src/areas/authentication/config/PassportConfig.ts
+++ src/areas/authentication/config/PassportConfig.ts
@@ -1,14 +1,77 @@
 import passport from 'passport';
 import { PassportStrategy } from '../../../interfaces/passport.strategy.interface';
+import { MockAuthenticationService } from '../services/Authentication.service.mock';
+import { Strategy as LocalStrategy, Strategy } from "passport-local";
 
+// Passport Configuration Class
 export default class PassportConfig {
-    constructor(strategies: PassportStrategy[]) {
-        this.addStrategies(strategies);
+    private _strategies: PassportStrategy[];
+    private _defaultStrategy: PassportStrategy;
+
+    constructor() {
+        this._strategies = [];
+        this._defaultStrategy = new LocalStrategyConfig().getConfiguredStrategy();
+        this.addStrategy(this._defaultStrategy);
     }
 
-    private addStrategies(strategies: PassportStrategy[]): void {
+    private addAllStrategies(strategies: PassportStrategy[]): void {
         strategies.forEach((passportStrategy: PassportStrategy) => {
             passport.use(passportStrategy.name, passportStrategy.strategy);
         });
+    }
+
+    public addStrategy(strategy: PassportStrategy): void {
+        this._strategies.push(strategy);
+        this.addAllStrategies(this._strategies);
+    }
+}
+
+// Local Strategy Configuration Class
+class LocalStrategyConfig {
+    private static _authService = new MockAuthenticationService;
+
+    private configStrategy(): Strategy {
+        return new LocalStrategy(
+            {
+                usernameField: "email",
+                passwordField: "password",
+            },
+            (email, password, done) => {
+                try {
+                    const user = LocalStrategyConfig._authService.getUserByEmailAndPassword(email, password);
+                    console.log("test tst");
+                    return done(null, user);
+                } catch (error) {
+                    return done(null, false, { message: error.message });
+                }
+            }
+        );
+    }
+
+    private serializeUser(): void {
+        passport.serializeUser(function (user: Express.User, done: ((error: any, email: string | undefined) => void)) {
+            done(null, user.email);
+        });
+    }
+
+    private deserializeUser(): void {
+        passport.deserializeUser(function (email: string, done: ((error: any, user: Express.User | false | null) => void)) {
+            try {
+                const user = LocalStrategyConfig._authService.findUserByEmail(email);
+                return done(null, user);
+            } catch (error) {
+                return done(error.message, undefined);
+            }
+        });
+    }
+
+    public getConfiguredStrategy(): PassportStrategy {
+        const strategy = this.configStrategy();
+        this.serializeUser();
+        this.deserializeUser();
+        return {
+            name: "local",
+            strategy: strategy
+        }
     }
 }

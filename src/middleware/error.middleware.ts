23509a1af56a338e61cdd3af8d059e427d5d4d78
Royce Coviello <coviello.royce@gmail.com>
2023-04-11 14:27:46 -0700

    fixed merge conflict

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -3,21 +3,15 @@ import HttpException from "../exceptions/HttpException";
 import fs from "fs";
 
 const errorMiddleware = (error: HttpException, request: Request, response: Response, next: NextFunction) => {
-<<<<<<< HEAD
-  // const status = error.status || 500;
-  // const message = error.message || "An error has occurred";
-  // const errorInfo = `${new Date().toLocaleString("en-US", { day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric", hour12: true })} - ${status} - ${message}\n`;
-  // fs.appendFile("error.log", errorInfo, (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  // });
-  // console.log("error logged");
-=======
-  const status = error.status || 500;
-  const message = error.message || "An error has occured";
-  console.log(status, message);
->>>>>>> 9b6a3343e2b5904736c6e7a1798b5d363719f38e
+    const status = error.status || 500;
+  const message = error.message || "An error has occurred";
+  const errorInfo = `${new Date().toLocaleString("en-US", { day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric", hour12: true })} - ${status} - ${message}\n`;
+  fs.appendFile("error.log", errorInfo, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  console.log("error logged");
 };
 
 export default errorMiddleware;
